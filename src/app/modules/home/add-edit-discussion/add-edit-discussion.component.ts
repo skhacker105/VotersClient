a8eb969b474591e9ac334facec7bd9f07eedabdd
skhacker105 <skhacker105@gmail.com>
2023-09-07 22:20:39 +0530

    discussion state, admin guard,  viting enable disable

--- src/app/modules/home/add-edit-discussion/add-edit-discussion.component.ts
+++ src/app/modules/home/add-edit-discussion/add-edit-discussion.component.ts
@@ -2,8 +2,11 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
+import { Discussion } from 'src/app/core/models/discussion';
+import { IUser } from 'src/app/core/models/user';
 import { DiscussionService } from 'src/app/core/services/discussion.service';
 import { LoggerService } from 'src/app/core/services/logger.service';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-add-edit-discussion',
@@ -14,6 +17,7 @@ export class AddEditDiscussionComponent implements OnInit, OnDestroy {
 
   id: string | null | undefined;
   isComponentIsActive = new Subject<boolean>();
+  loginProfile: IUser | undefined;
   discussionForm = new FormGroup<any>({
     title: new FormControl<string>('', Validators.required),
     message: new FormControl<string>('', Validators.required)
@@ -23,11 +27,13 @@ export class AddEditDiscussionComponent implements OnInit, OnDestroy {
     private route: ActivatedRoute,
     private discussionService: DiscussionService,
     private loggerService: LoggerService,
-    private router: Router
+    private router: Router,
+    private userService: UserService
   ) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
+    this.loginProfile = this.userService.getProfile();
     this.getDiscussion()
   }
 
@@ -43,12 +49,23 @@ export class AddEditDiscussionComponent implements OnInit, OnDestroy {
       .pipe(takeUntil(this.isComponentIsActive))
       .subscribe({
         next: res => {
-          this.discussionForm.patchValue(res.data)
+          if (this.checkIfOwner(res.data))
+            this.discussionForm.patchValue(res.data)
+          else {
+            this.loggerService.showError('Not your discussion to edit.')
+            this.router.navigateByUrl('/');
+          }
         },
         error: err => {
           this.loggerService.showError(err.error.message);
         }
       });
+  }
+
+  checkIfOwner(d: Discussion): boolean {
+    if (!this.loginProfile) return false;
+    if (d.createdBy._id !== this.loginProfile._id) return false;
+    return true;
   }
 
   saveDiscussion() {
@@ -69,7 +86,7 @@ export class AddEditDiscussionComponent implements OnInit, OnDestroy {
           this.router.navigate(['/home/discussionDetail/', res.data._id])
         },
         error: err => {
-          this.loggerService.showError(err.error.message);
+          this.loggerService.showError(err.error.error);
         }
       });
   }
